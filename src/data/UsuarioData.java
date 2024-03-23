2d63fa229f791e413ad6e8f727b9c62f7cfbb904
LucieneCavalcanti <lucienecavalcanti@live.com>
2024-03-23 14:16:16 -0300

    UsuarioData com incluir,editar, excluir

--- src/data/UsuarioData.java
+++ src/data/UsuarioData.java
@@ -1,5 +1,6 @@
 package data;
 
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 
 import model.UsuarioModel;
@@ -9,20 +10,43 @@ public class UsuarioData extends Conexao implements CRUD {
 
     @Override
     public boolean editar(UsuarioModel obj) throws Exception {
-        // TODO Auto-generated method stub
-        return false;
+        String sql="update tbusuarios set nome=?,email=?,senha=? where id=?";
+        PreparedStatement ps = getCon().prepareStatement(sql);
+        ps.setString(1, obj.getNome());
+        ps.setString(2, obj.getEmail());
+        ps.setString(3, obj.getSenha());
+        ps.setInt(4,obj.getId());
+        //return (ps.executeUpdate()>0); //insert,update,delete
+        //ou
+        //if (ps.executeUpdate()>0) return true;
+        //else return false;
+        return (ps.executeUpdate()>0) ? true : false;
     }
 
     @Override
     public boolean excluir(int id) throws Exception {
-        // TODO Auto-generated method stub
-        return false;
+        String sql="delete from tbusuarios where id=?";
+        PreparedStatement ps = getCon().prepareStatement(sql);
+        ps.setInt(1, id);
+        //return (ps.executeUpdate()>0); //insert,update,delete
+        //ou
+        //if (ps.executeUpdate()>0) return true;
+        //else return false;
+        return (ps.executeUpdate()>0) ? true : false;
     }
 
     @Override
     public boolean incluir(UsuarioModel obj) throws Exception {
-        // TODO Auto-generated method stub
-        return false;
+        String sql="insert into tbusuarios (nome,email,senha) values(?,?,?)";
+        PreparedStatement ps = getCon().prepareStatement(sql);
+        ps.setString(1, obj.getNome());
+        ps.setString(2, obj.getEmail());
+        ps.setString(3, obj.getSenha());
+        //return (ps.executeUpdate()>0); //insert,update,delete
+        //ou
+        //if (ps.executeUpdate()>0) return true;
+        //else return false;
+        return (ps.executeUpdate()>0) ? true : false; //if ternário
     }
 
     @Override
